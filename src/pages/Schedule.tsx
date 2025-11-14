5d1cfe77d5eea22e08119afe2ca172850d7d1389
ashish086 <47134160+ashish086@users.noreply.github.com>
2025-11-14 23:15:32 +0530

    Remove unused import of dayjs from Schedule.tsx

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import "../styles/schedule.css";
 import { X } from "lucide-react";
-
-import dayjs, { Dayjs } from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
