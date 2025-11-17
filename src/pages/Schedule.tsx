18e03170f21e42c09c1a56f11c10bbcde6e2b151
Roshan <roshansingh9641460041@gmail.com>
2025-11-18 01:34:56 +0530

    firstcommit

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../styles/schedule.css";
 import { X } from "lucide-react";
 
-import dayjs, { Dayjs } from "dayjs";
+import { Dayjs } from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
